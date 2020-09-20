848d6b7ecbdca635c4585980f9877bfef867201e
yakohere <yaxyo.ismoiljonov@teamuni.uz>
2020-09-20 22:19:48 +0500

    apply middleware

---
+++ dist/routes/todos.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+simport;
+{
+    Router;
+}
+from;
+"express";
+const router = Router();
+router.post("/");
+router.get("/");
+router.patch("/:id");
+router.delete("/:id");
+exports.default = router;
